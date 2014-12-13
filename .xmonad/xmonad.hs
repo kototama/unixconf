1b49222bc2a06dbd1fc875b88a26b298bae75398
kototama <kototamo@gmail.com>
2014-12-13 16:45:29 +0100

    xmonad conf

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,15 @@
+import XMonad.Config.Xfce
+import XMonad
+
+import XMonad.Config.Azerty
+
+import qualified Data.Map as M
+ 
+main = xmonad xfceConfig
+       { terminal = "xfce4-terminal"
+       , modMask = mod1Mask -- sets to alt key 
+       , borderWidth = 1 --was "3"
+       , focusedBorderColor = "#4099FF"
+       , normalBorderColor = "#474747"
+       , keys = \c -> azertyKeys c `M.union` keys xfceConfig c
+       }
