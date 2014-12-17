d3da7e6bb74ee46dca7837481d1e266d5ff6b2ca
Kototama <kototamo@gmail.com>
2014-12-17 16:16:35 +0100

    git findbigfiles script

---
+++ local/bin/git-findbigfiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# http://naleid.com/blog/2012/01/17/finding-and-purging-big-files-from-git-history
+
+git rev-list --objects --all | sort -k 2 > allfileshas.txt
+
+git gc && git verify-pack -v .git/objects/pack/pack-*.idx | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+$" | sort -k 3 -n -r > bigobjects.txt
+
+for SHA in `cut -f 1 -d\  < bigobjects.txt`; do
+echo $(grep $SHA bigobjects.txt) $(grep $SHA allfileshas.txt) | awk '{print $1,$3,$7}' > bigtosmall.txt
+done;
