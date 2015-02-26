65e87bc6bfa9948045d2c105e4d6d94b0d19f545
kototama <kototamo@gmail.com>
2015-02-26 10:32:39 +0100

    fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,7 +22,7 @@ end
 
 function fish_prompt
   if test -d .git
-    printf '%s@%s %s%s%s:%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
+    printf '%s@%s %s%s%s (%s)\n> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
   else
     printf '%s@%s %s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
   end
