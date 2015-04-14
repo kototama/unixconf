f0154202817c968b7536bdbc1508916d5ea0f4ab
kototama <kototamo@gmail.com>
2015-04-14 16:58:49 +0200

    add eggs to PYTHONPATH for jedi completion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,11 @@ set -gx PATH /home/alphonse/projects/adhocracy3.mercator/bin ~/.local/bin $PATH
 set -gx A3_ROOT /home/alphonse/projects/adhocracy3.mercator
 set -gx LD_LIBRARY_PATH /home/alphonse/projects/adhocracy3.mercator/python/parts/opt/lib/
 
+set -gx PYTHONPATH /home/alphonse/projects/adhocracy3.mercator/src /home/alphonse/projects/adhocracy3.mercator/src/substanced/ $A3_ROOT/eggs/pyramid-*.egg/ $A3_ROOT/eggs/*.egg $PYTHONPATH
+
 set -gx EDITOR "emacsclient -a=emacs"
+
+set --erase fish_greeting
 
 set fish_git_dirty_color red
 set fish_git_not_dirty_color green
