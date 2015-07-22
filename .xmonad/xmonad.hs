842be27fe76220b0b271cacd44884af010e4d139
kototama <kototamo@gmail.com>
2015-07-22 16:23:40 +0200

    xmonad specific work configuration

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,13 +1,15 @@
-import XMonad.Config.Xfce
-import XMonad
-
-import XMonad.Config.Azerty
-
 import qualified Data.Map as M
 
+import Network.HostName (getHostName)
+import XMonad.Config.Xfce
+import XMonad
+import XMonad.Config.Azerty
 import XMonad.Util.EZConfig
 
-theManageHook = composeAll [
+
+myManageHook = composeAll [
+  className =? "Galculator" --> doShift "2:programming",
+  className =? "Emacs" --> doShift "2:programming",
   className =? "Firefox" --> doShift "3:web",
   className =? "Pidgin" --> doShift "4:chat"
   ]
@@ -19,14 +21,20 @@ myKeys = [--  ((mod4Mask, xK_semicolon), sendMessage (IncMasterN (-1)))
          -- , ((mod4Mask .|. shiftMask, xK_c), spawn "emacsclient")
          ]
 
---       , modMask = mod1Mask -- sets to alt key
-main = xmonad $ xfceConfig
-       { terminal = "xfce4-terminal"
-       , workspaces = ["1:xfce","2:work","3:web","4:chat","5:private","6","7","8","9","0"]
-       , modMask = mod4Mask
-       , borderWidth = 1 --was "3"
-       , focusedBorderColor = "#4099FF"
-       , normalBorderColor = "#474747"
-       , manageHook = theManageHook <+> manageHook xfceConfig
-       , keys = \c -> azertyKeys c `M.union` keys xfceConfig c
-       } `additionalKeys` myKeys
+myConfig hostname = xfceConfig { terminal = "xfce4-terminal"
+                      , workspaces = ["1:xfce","2:programming","3:web","4:chat","5:private","6","7","8","9","0"]
+                      , modMask = mod4Mask
+                      , borderWidth = 1
+                      , focusedBorderColor = "#4099FF"
+                      , normalBorderColor = "#474747"
+                      , manageHook = theManageHook
+                      , keys = \c -> azertyKeys c `M.union` keys xfceConfig c
+                      } `additionalKeys` myKeys
+                    where theManageHook = case hostname of
+                            "adorno" -> myManageHook <+> manageHook xfceConfig
+                            _ -> manageHook xfceConfig
+           --       , modMask = mod1Mask -- sets to alt key
+           
+main = do
+  hostname <- getHostName
+  xmonad $ (myConfig hostname)
