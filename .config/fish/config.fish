77f6adc6c1a696d413a3e13c016182ef75d79fd6
kototama <kototamo@gmail.com>
2015-12-10 15:45:50 +0100

    java configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,14 @@
 alias e "emacs"
 alias ec "emacsclient -n -a=emacs"
-alias pacup "sudo pacman -Syu"
-alias aup "yaourt -Syu --aur"
+alias pacup "sudo etckeeper pre-install; and sudo pacman -Syu; and sudo etckeeper post-install"
+alias aup "sudo etckeeper pre-install; and yaourt -Syu --aur; and sudo etckeeper post-install"
 
 set -gx PYENV_ROOT ~/.pyenv
 
 set -gx PATH $PYENV_ROOT/bin ~/.local/bin ~/local/bin $PATH
 
-
+# for XMonad with Java applications
+set -gx _JAVA_AWT_WM_NONREPARENTING=1
 
 # ~/.stack/programs/x86_64-linux/ghc-7.8.4/bin $PATH
 
