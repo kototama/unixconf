06a780a75f3a9584c861526e314e56034c1d5650
kototama <kototamo@gmail.com>
2016-06-05 11:21:53 +0200

    xmonad without xfce

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,10 @@
 import qualified Data.Map as M
 import System.Posix.Unistd (getSystemID, nodeName)
 
-import XMonad.Config.Xfce
 import XMonad
 import XMonad.Config.Azerty
 import XMonad.Util.EZConfig
-
+import XMonad.Hooks.DynamicLog
 
 myManageHook = composeAll [
   className =? "Galculator" --> doShift "2:programming",
@@ -22,18 +21,18 @@ myKeys = [--  ((mod4Mask, xK_semicolon), sendMessage (IncMasterN (-1)))
          -- , ((mod4Mask .|. shiftMask, xK_e), spawn "galculator")
          ]
 
-myConfig hostname = xfceConfig { terminal = "xfce4-terminal"
+myConfig hostname = defaultConfig { terminal = "xfce4-terminal"
                       , workspaces = ["1:xfce","2:programming","3:web","4:chat","5:private","6","7","8","9","0"]
                       , modMask = mod4Mask
                       , borderWidth = 1
                       , focusedBorderColor = "#4099FF"
                       , normalBorderColor = "#474747"
                       , manageHook = theManageHook
-                      , keys = \c -> azertyKeys c `M.union` keys xfceConfig c
+                      , keys = \c -> azertyKeys c `M.union` keys defaultConfig c
                       } `additionalKeys` myKeys
                     where theManageHook = case hostname of
-                            "adorno" -> myManageHook <+> manageHook xfceConfig
-                            _ -> manageHook xfceConfig
+                            "adorno" -> myManageHook <+> manageHook defaultConfig
+                            _ -> manageHook defaultConfig
            --       , modMask = mod1Mask -- sets to alt key
 
 getHostName :: IO String
@@ -41,4 +40,4 @@ getHostName = fmap nodeName getSystemID
 
 main = do
   hostname <- getHostName
-  xmonad $ (myConfig hostname)
+  xmonad =<< dzen (myConfig hostname)
